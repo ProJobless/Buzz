183d761556aee439414703a976fba9d6a1f03758
Aayush Ranaut <aayushranaut@liquidserve.com>
2013-07-21 17:07:28 +0530

    Scheduled tweets Cron added ;)

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -2,15 +2,30 @@
 
 class Cron extends CI_Controller
 {
-	function index()
+	/*
+		Function will post the scheduled tweets
+		Frequency : 1 minute
+	*/
+	function scheduled_tweets()
 	{
 		//Post the scheduled tweet
+		$time = time() + 59;
+		$this->db->from('scheduled_tweets')->where('scheduled_time <=', $time)->order_by('scheduled_time', 'asc');
 		
+		$query = $this->db->get();
 		
+		foreach($query->result() as $r)
+		{
+			post_twitter($r->tweet,$r->reply_id); //Will add a helper later
+			
+			// Now add the record in the DB
+			 
+		}
 	}
 	
 	/*
 		Function will update the stats every day for admin panel!
+		Frequency : 24 hours
 	*/
 	function update_admin_stats()
 	{
@@ -18,6 +33,7 @@ class Cron extends CI_Controller
 	}
 	/*
 		Function will generate invoice for clients to pay 10 days in advance
+		Frequency : 24 hours
 	*/
 	function generate_invoices()
 	{
@@ -25,6 +41,7 @@ class Cron extends CI_Controller
 	}
 	/*
 		Function will send emails depending upon priority
+		Frequency : 5 minutes
 	*/
 	function send_emails()
 	{
