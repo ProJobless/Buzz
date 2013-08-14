3579220dfb3dce8e14c032fd55ffc9c748279724
Aayush Ranaut <aayush.ranaut@gmail.com>
2013-08-14 16:23:44 +0530

    Updated admincp email

--- application/views/admincp/edit_email.php
+++ application/views/admincp/edit_email.php
@@ -39,6 +39,27 @@
 							</div>
 							<input type="hidden" name="id" value="<?php echo $this->uri->segment(4); ?>">
 						</div>
+						<div class="row-fluid">
+							<div class="box">
+								<div class="box-header">
+									<span class="title">Available Email Variables</span>
+								</div>
+								<div class="box-content padded">
+									<p>{$username} 			= Username of the user</p>
+									<p>{$email}				= Email of the user</p>
+									<p>{$first_name}		= First name of user</p>
+									<p>{$last_name}			= Last name of the user</p>
+									<p>{$plan_id} 			= Plan ID</p>
+									<p>{$company}			= Company of the user</p>
+									<p>{$invoice_id}		= Invoice ID</p>
+									<p>{$ticket_id} 		= Ticket ID</p>
+									<p>{$ticket_body}		= Body of the opened ticket</p>
+									<p>{$ticket_reply_id} 	= Reply ID</p>
+									<p>{$ticket_reply}		= Reply to the ticket by Admin</p>
+								</div>
+							</div>
+						</div>
+						
 						<div class="form-actions">
 							<button type="submit" class="btn btn-blue">Save Template</button>			  
 						</div>
